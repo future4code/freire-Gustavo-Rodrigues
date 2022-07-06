48a941dffa0b59e90f0c4a3929b36c577451aa39
Gustavo Willian <gustavowillianfr@outlook.com>
2022-07-05 22:25:34 -0300

    Consegui resolver algumas coisas, mas esta dando o erro

--- modulo3/useEffect/aula32-use-effect/src/App.js
+++ modulo3/useEffect/aula32-use-effect/src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
-import './App.css';
+import React, {useState, useEffect} from "react";
+import './styles.css';
+import axios from "axios";
+import PokeCard from "./components/PokeCard/PokeCard";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+const App = () => {
+
+  const [pokeList, setPokeList] = useState ([])
+  const [pokeName, setPokeName] = useState ("")
+
+
+  const getPokemon = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151%22")
+      .then(response => {
+        setPokeList(response.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const changePokeName = event => {
+    setPokeName(event.target.value);
+  };
+
+  useEffect (() => {
+    getPokemon()
+  }, [])
+
+    return (
+      <div className="App">
+
+        <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+
+        {pokeName && <PokeCard pokemon={pokeName} />}
+      </div>
+    );
+  }
 
 export default App;
