16bebe81986a236a0d02f0ecb630f4ec375bffd4
GustavoWillianFr <gustavowillianfr@outlook.com>
2022-08-10 19:39:33 -0300

    Finalizei o exercicio de hoje

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,10 @@
+function imprimeTresCoresFavoritas(primeiraCor: string, segundaCor: string, terceiraCor: string): void {
+    const cor1 = primeiraCor
+    const cor2 = segundaCor
+    const cor3 = terceiraCor
+    console.log([cor1, cor2, cor3])
+
+ }
+
+  imprimeTresCoresFavoritas("azul", "verde", "roxo")
+
